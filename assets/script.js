4d0fbff3426ceb2204257b29a47eac06240783ec
Freek1ll3R <gugakobakhidze18@gmail.com>
2023-08-08 01:10:40 +0400

    Add new project from figma

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const gridCards = document.querySelectorAll(".grid_card");
+
+  gridCards.forEach((card) => {
+    card.addEventListener("click", function () {
+      card.classList.toggle("hovered");
+
+      
+      gridCards.forEach((otherCard) => {
+        if (otherCard !== card) {
+          otherCard.classList.remove("hovered");
+        }
+      });
+    });
+  });
+});
